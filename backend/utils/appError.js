acfa8f2f0f9bf5de298432772ad792dd6a166d49
Shankaranarayanan <sankaranarayananit002@gmail.com>
2024-08-30 18:58:34 +0530

    urls

--- backend/utils/appError.js
+++ backend/utils/appError.js
@@ -10,3 +10,7 @@ class createError extends Error{
 }
 
 module.exports = createError;
+
+// SECRET_KEY = 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
+// DATABASE_URL = mongodb+srv://sankaranarayananit002:FCsR1J3kHd4dYtmW@vsbec-vechile.zinj7.mongodb.net/
+ 
